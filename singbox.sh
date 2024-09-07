a542ede4a5737c8cc68f8b972fb344e675a20bca
789-ooo <789789789@smoe.cloudns.org>
2024-09-07 11:48:53 +0800

    Update singbox.sh

--- singbox.sh
+++ singbox.sh
@@ -228,7 +228,7 @@ cat > "${config_dir}" << EOF
                     "server": "www.iij.ad.jp",
                     "server_port": 443
                 },
-                "private_key": "$private_key",
+                "private_key": "$kcptun",
                 "short_id": [
                   ""
                 ]
