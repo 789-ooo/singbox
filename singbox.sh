e1611df2c1f78ba216cb1046074768e650987265
789-ooo <789789789@smoe.cloudns.org>
2024-09-07 11:59:55 +0800

    Update singbox.sh

--- singbox.sh
+++ singbox.sh
@@ -240,8 +240,6 @@ cat > "${config_dir}" << EOF
         "type": "vmess",
         "listen": "::",
         "listen_port": 8001,
-	"sniff": false,
-        "sniff_override_destination": false,
         "users": [
         {
             "uuid": "$uuid"
